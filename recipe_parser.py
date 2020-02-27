30a8ae871e2ff1c3fc027433c9c8337951038ebe
Connor Tincher <connortincher2021@u.northwestern.edu>
2020-02-26 21:36:45 -0600

    Deleted parser_old.py, moved parser.py to recipe_parser.py

---
+++ recipe_parser.py
@@ -0,0 +1,20 @@
+from recipe_fetcher import RecipeFetcher
+import ingredient_parser as ingred_parse
+import tool_parser as tool_parse
+import method_parser as method_parse
+import step_parser as step_parse
+
+
+def parsed_recipe(url):
+    rf = RecipeFetcher()
+    scraped = rf.scrape_recipe(url)
+    ingredients = ingred_parse.scrape_ingredients(scraped['ingredients'])
+    tools = tool_parse.scrape_tools(scraped['directions'])
+    methods = method_parse.scrape_methods(scraped)
+    steps = step_parse.scrape_steps(scraped)
+
+    recipe = {'ingredients': ingredients,
+              'tools': tools,
+              'methods': methods,
+              'steps': steps}
+    return recipe
