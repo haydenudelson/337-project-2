34ec20e89d61efac05a1f084567e763f65b85cb9
Connor Tincher <connortincher2021@u.northwestern.edu>
2020-03-04 22:17:38 -0600

    Changes made to the recipe are now tracked, and displayed after each transformation.

--- transformation.py
+++ transformation.py
@@ -1,15 +1,15 @@
 import copy
 
 sample_recipe = {
-    'ingredients':{
-        'name':{
-            'quantity':'',
-            'measurement':''
+    'ingredients': {
+        'name': {
+            'quantity': '',
+            'measurement': ''
         }
     },
     'tools': ["knife", "pan"],
-    'methods':['boil'],
-    'steps':['']
+    'methods': ['boil'],
+    'steps': ['']
 }
 
 # From, in part, Wikipedia's List of Meat Substitutes
@@ -20,8 +20,8 @@ to_vegan_list = {
     "mozzarella cheese": "daiya mozzarella",
     "scrambled egg": "tofu scramble",
     "egg": "oat flour", "eggs": "oat flour",
-    "butter":"olive oil",
-    "cheese":"tofu (crumbled)",
+    "butter": "olive oil",
+    "cheese": "tofu (crumbled)",
 
 }
 
@@ -32,98 +32,98 @@ to_vegetarian_list = {
     "chicken broth": "vegetable broth",
     "beef broth": "vegetable broth",
     "pork broth": "vegetable broth",
-    "chicken bouillon":"vegetable bouillon",
-    "beef bouillon":"vegetable bouillon",
-    "pork bouillon":"vegetable bouillon",
+    "chicken bouillon": "vegetable bouillon",
+    "beef bouillon": "vegetable bouillon",
+    "pork bouillon": "vegetable bouillon",
     "meatball": "veggie meatball",
     "sausage link": "veggie sausage link",
-    "bacon":"veggie bacon",
-    "burger":"veggie burger",
-    "hamburger":"veggie burger",
-    "sausage":"glamorgan sausage",
-    "pork":"jackfruit",
-    "pepperoni":"vegetable deli slice",
-    "pastrami":"vegetable deli slice",
-    "chicken breast":"tofu",
-    "chicken thigh":"tofu",
-    "chicken nuggets":"soy nuggets",
-    "chicken":"tempeh",
-    "beef":"tofurkey",
-    "ground beef":"soy protein",
-    "steak":"portebllo mushrooms",
-    "ribs":"portebello mushrooms",
-    "veal":"tofurkey",
-    "lamb":"tofurkey",
-    "turkey":"tofurkey",
-    "tuna":"tempeh",
-    "salmon":"tempeh",
+    "bacon": "veggie bacon",
+    "burger": "veggie burger",
+    "hamburger": "veggie burger",
+    "sausage": "glamorgan sausage",
+    "pork": "jackfruit",
+    "pepperoni": "vegetable deli slice",
+    "pastrami": "vegetable deli slice",
+    "chicken breast": "tofu",
+    "chicken thigh": "tofu",
+    "chicken nuggets": "soy nuggets",
+    "chicken": "tempeh",
+    "beef": "tofurkey",
+    "ground beef": "soy protein",
+    "steak": "portebllo mushrooms",
+    "ribs": "portebello mushrooms",
+    "veal": "tofurkey",
+    "lamb": "tofurkey",
+    "turkey": "tofurkey",
+    "tuna": "tempeh",
+    "salmon": "tempeh",
     "scallop": "king oyster mushroom",
-    "spam":"soy protein",
-    "crab":"tofu",
-    "haddock":"tempeh",
-    "cod":"tempeh",
-    "mackerel":"tempeh"
+    "spam": "soy protein",
+    "crab": "tofu",
+    "haddock": "tempeh",
+    "cod": "tempeh",
+    "mackerel": "tempeh"
 }
 
-from_vegetarian_list ={
-    "vegetable broth":"chicken broth",
-    "vegetable bouillon":"chicken bouillon"
+from_vegetarian_list = {
+    "vegetable broth": "chicken broth",
+    "vegetable bouillon": "chicken bouillon"
 }
 
-#based in part on https://www.thespruceeats.com/chinese-cooking-ingredient-substitutions-4057957
+# based in part on https://www.thespruceeats.com/chinese-cooking-ingredient-substitutions-4057957
 to_chinese_list = {
-    "olive oil":"peanut oil",
-    "vegetable oil":"peanut oil",
-    "canola oil":"peanut oil",
-    "coconut oil":"peanut oil",
-    "pasta":"lo mein",
-    "spaghetti":"lo mein",
-    "fettucini":"lo mein",
-    "penne":"lo mein",
-    "chives":"green onion",
-    "parsley":"green onion",
-    "basil":"green onion",
-    "worcestershire sauce":"soy sauce",
-    "hot sauce":"chili sauce",
-    "red pepper":"chili paste",
-    "vinegar":"chinkiang vinegar",
-    "broccoli":"bok choy",
-    "cabbage":"bok choy",
-    "gelatin":"agar-agar",
-    "carrot":"bamboo shoot",
-    "asparagus":"bamboo shoot",
-    "whole milk":"coconut milk",
-    "milk":"coconut milk",
-    "half-and-half":"coconut cream",
-    "whipping cream":"coconut cream",
-    "cornstarch":"lotus root flour",
-    "oyster sauce":"soy sauce",
-    "sherry vinegar":"rice vinegar"
+    "olive oil": "peanut oil",
+    "vegetable oil": "peanut oil",
+    "canola oil": "peanut oil",
+    "coconut oil": "peanut oil",
+    "pasta": "lo mein",
+    "spaghetti": "lo mein",
+    "fettucini": "lo mein",
+    "penne": "lo mein",
+    "chives": "green onion",
+    "parsley": "green onion",
+    "basil": "green onion",
+    "worcestershire sauce": "soy sauce",
+    "hot sauce": "chili sauce",
+    "red pepper": "chili paste",
+    "vinegar": "chinkiang vinegar",
+    "broccoli": "bok choy",
+    "cabbage": "bok choy",
+    "gelatin": "agar-agar",
+    "carrot": "bamboo shoot",
+    "asparagus": "bamboo shoot",
+    "whole milk": "coconut milk",
+    "milk": "coconut milk",
+    "half-and-half": "coconut cream",
+    "whipping cream": "coconut cream",
+    "cornstarch": "lotus root flour",
+    "oyster sauce": "soy sauce",
+    "sherry vinegar": "rice vinegar"
 }
 
-#https://brighamhealthhub.org/healthy-living/ten-simple-substitutes-for-healthy-eating
-#https://greatist.com/health/83-healthy-recipe-substitutions#Gluten-Free-Swaps
+# https://brighamhealthhub.org/healthy-living/ten-simple-substitutes-for-healthy-eating
+# https://greatist.com/health/83-healthy-recipe-substitutions#Gluten-Free-Swaps
 to_healthy_list = {
-    "white rice":"brown rice",
-    "egg":"egg white", "eggs":"egg whites",
-    "pasta":"multigrain pasta",
-    "spaghetti":"multigrain spaghetti",
-    "cheese":"low-fat cheese",
-    "sour cream":"fat-free yogurt",
-    "cream":"skim milk",
-    "flour":"whole wheat flour",
-    "all-purpose flour":"whole wheat flour",
-    "couscous":"quinoa",
-    "bread crumbs":"ground flaxseeds",
-    "tortilla":"lettuce leaves", "tortillas":"lettuce leaves",
-    "oatmeal":"quinoa",
-    "crouton":"almond", "croutons":"almonds",
-    "chocolate chip":"cacao nib",
-    "white wine":"red wine",
-    "milk":"almond milk",
-    "butter":"olive oil",
-    "vegetable oil":"olive oil",
-    "canola oil":"olive oil"
+    "white rice": "brown rice",
+    "egg": "egg white", "eggs": "egg whites",
+    "pasta": "multigrain pasta",
+    "spaghetti": "multigrain spaghetti",
+    "cheese": "low-fat cheese",
+    "sour cream": "fat-free yogurt",
+    "cream": "skim milk",
+    "flour": "whole wheat flour",
+    "all-purpose flour": "whole wheat flour",
+    "couscous": "quinoa",
+    "bread crumbs": "ground flaxseeds",
+    "tortilla": "lettuce leaves", "tortillas": "lettuce leaves",
+    "oatmeal": "quinoa",
+    "crouton": "almond", "croutons": "almonds",
+    "chocolate chip": "cacao nib",
+    "white wine": "red wine",
+    "milk": "almond milk",
+    "butter": "olive oil",
+    "vegetable oil": "olive oil",
+    "canola oil": "olive oil"
 }
 
 
@@ -140,8 +140,6 @@ class Transformer:
         # look at list of ingredients for meats
         # find corresponding step and replace meat ingredient with appropriate substitute
 
-        # print(recipe["steps"])
-
         ingredients = recipe["ingredients"].keys()
         changed_ingredients = []
         for i in ingredients:
@@ -152,9 +150,7 @@ class Transformer:
             new_i = changed[1]
             recipe = self.replace_ingredient(recipe, old_i, new_i)
 
-        # print(recipe["steps"])
-
-        return recipe
+        return recipe, changed_ingredients
 
     def from_vegetarian(self, recipe):
         ingredients = recipe["ingredients"].keys()
@@ -168,7 +164,7 @@ class Transformer:
             new_i = changed[1]
             recipe = self.replace_ingredient(recipe, old_i, new_i)
 
-        return recipe
+        return recipe, changed_ingredients
 
     def to_healthy(self, recipe):
         ingredients = recipe["ingredients"].keys()
@@ -182,10 +178,10 @@ class Transformer:
             new_i = changed[1]
             recipe = self.replace_ingredient(recipe, old_i, new_i)
 
-        return recipe
+        return recipe, changed_ingredients
 
     def from_healthy(self, recipe):
-        return recipe
+        return recipe, 'None'
 
     def to_chinese(self, recipe):
         ingredients = recipe["ingredients"].keys()
@@ -199,27 +195,26 @@ class Transformer:
             new_i = changed[1]
             recipe = self.replace_ingredient(recipe, old_i, new_i)
 
-        return recipe
+        return recipe, changed_ingredients
 
     def double(self, recipe):
         for ingredient in recipe['ingredients']:
             quantity = recipe['ingredients'][ingredient]['Quantity']
             if isinstance(quantity, int) or isinstance(quantity, float):
                 recipe['ingredients'][ingredient]['Quantity'] = quantity * 2
-        return recipe
+        return recipe, 'Doubled everything'
 
     def halve(self, recipe):
         for ingredient in recipe['ingredients']:
             quantity = recipe['ingredients'][ingredient]['Quantity']
             if isinstance(quantity, int) or isinstance(quantity, float):
                 recipe['ingredients'][ingredient]['Quantity'] = quantity * 0.5
-        return recipe
+        return recipe, 'Halved everything'
 
     def to_vegan(self, recipe):
-        recipe = self.to_vegetarian(recipe)
+        recipe, changed_ingredients = self.to_vegetarian(recipe)
         ingredients = recipe["ingredients"].keys()
 
-        changed_ingredients = []
         for i in ingredients:
             if i in to_vegan_list:
                 changed_ingredients.append([i, to_vegan_list[i]])
@@ -228,4 +223,4 @@ class Transformer:
             new_i = changed[1]
             recipe = self.replace_ingredient(recipe, old_i, new_i)
 
-        return recipe
+        return recipe, changed_ingredients
